439645b9c3c2cf319c194d0b4066465eef3a64a2
Aryanvaish <108726947+Aryanvaish@users.noreply.github.com>
2025-11-12 20:54:04 +0530

    sitemap added

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,44 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://aryanvaish-portfolio.vercel.app",
+
   generateRobotsTxt: true,
+
+  exclude: ["/404", "/500"],
+
+  changefreq: "weekly",
+  priority: 0.7, // medium priority for all pages
+
+  sitemapSize: 5000,
+
+  generateIndexSitemap: false,
+
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*", // All crawlers
+        allow: "/", // Allow all pages
+      },
+      {
+        userAgent: "GPTBot", // Example of a selective bot policy (optional)
+        disallow: ["/private"], // block specific routes if needed
+      },
+    ],
+    additionalSitemaps: [
+      "https://aryanvaish-portfolio.vercel.app/sitemap.xml", // ensures Google reads it
+    ],
+  },
+
+  transform: async (config, path) => {
+    if (path.includes("/admin")) {
+      return null;
+    }
+
+    return {
+      loc: path, // page URL
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+    };
+  },
 };
